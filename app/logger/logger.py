fae6174d66ca09baac7fe8b881f234f2c80555ed
rud-wtag <127865276+rud-wtag@users.noreply.github.com>
2024-04-23 17:49:52 +0600

    loguru setup

---
+++ app/logger/logger.py
@@ -0,0 +1,12 @@
+from loguru import logger
+
+logger.remove(0)
+
+logger.add(
+  "loguru.log",
+  level='DEBUG',
+  rotation="1 week",
+  retention="30 days",
+  compression="zip",
+  serialize=True,
+)
